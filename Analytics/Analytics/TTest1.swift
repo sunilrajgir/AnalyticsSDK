612f9f9ef628601a4e8a12194d10485f2d0503ee
Sunil Kumar29 <sunil.kumar29@olacabs.com>
2021-02-19 21:47:21 +0530

    generic analytics sdk event

---
+++ Analytics/Analytics/TTest1.swift
@@ -0,0 +1,67 @@
+//
+//  TTest1.swift
+//  Analytics
+//
+//  Created by Sunil Kumar29 on 19/02/21.
+//
+
+import Foundation
+
+
+class Test11 {
+    init() {
+        TTAnalyticsManager.shared.track(event: TTEvent(name: "abc", params: [:]))
+    }
+}
+
+class Test22 {
+    init() {
+        TTAnalyticsManager.shared.track(event: TTEvent(name: "xyz", params: [:]))
+    }
+}
+
+struct TTEvent: TTEventProtocol{
+    var name: String
+    var params: [String : Any]
+
+}
+
+protocol TTEventProtocol {
+    var name:String{get set}
+    var params:[String:Any]{get set}
+}
+
+protocol TTAnalyticsProtocol {
+    func track(event:TTEventProtocol)
+}
+
+final class TTAnalyticsManager: TTAnalyticsProtocol {
+    static let shared = TTAnalyticsManager()
+    var loggers = [TTLoggerProtocol]()
+    private init() {
+        loggers.append(TTFirebase())
+        loggers.append(TTFacebook())
+    }
+
+    func track(event: TTEventProtocol) {
+        for logger in loggers{
+            logger.track(event: event)
+        }
+    }
+}
+
+protocol TTLoggerProtocol {
+    func track(event:TTEventProtocol)
+}
+
+class TTFirebase: TTLoggerProtocol {
+    func track(event: TTEventProtocol) {
+        
+    }
+}
+
+class TTFacebook:  TTLoggerProtocol{
+    func track(event: TTEventProtocol) {
+        
+    }
+}
